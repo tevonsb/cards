fb4b47a30dbb3aef3a50da7e83d7c2059b5cfee8
Tevon Strand-Brown <tevon.strandbrown@gmail.com>
2019-05-31 16:25:43 -0700

    changed back

--- client/src/App.js
+++ client/src/App.js
@@ -1,103 +1,26 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { cards } from './cards.js';
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
-
-const title = "Black and White Cards";
-
-
-const description = ["The purpose of these cards is to jog the creative mindset. Sometimes we get stuck in the way we think about thing, the hope is that in considering something tangential, we get a different view of the problem at home. These were developed from interviews during the Stanford class Beyond Pink and Blue, and thus have a specific focus on gender in tech."]
-
-async function loadCards() {
-  console.log('loading cards...');
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM cards');
-    const results = { 'results': (result) ? result.rows : null};
-    console.log(results);
-    console.log('NO ERRORS IN DB CONNECTION')
-    client.release();
-  } catch(err) {
-    console.log('DB ERROR')
-    console.log(err);
-  }
-}
-
-
-class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state ={
-      cardsActive: false,
-      counter: 0,
-      card: cards[Math.floor(Math.random() * cards.length)],
-    }
-    this.selectCard = this.selectCard.bind(this);
-  }
-
-  componentDidMount(){
-    loadCards();
-  }
-
-  selectCard(){
-    const card = Math.floor(Math.random() * cards.length)
-    this.setState({
-      card: cards[card],
-    });
-    console.log('Cleckied')
-  }
-
-  renderCards(){
-    return (<div className="CardContainer"
-    >
-    <div className="Card"
-      onClick={() => this.setState({
-        card: cards[Math.floor(Math.random() * cards.length)],
-      })}
-      >{this.state.card}</div>
-  </div>)
-}
-
-getCard(){
-  return description.map((item) => {
-    return (<p key={item} className="Description">{item}</p>)
-  });
-}
-
-render() {
-  if(this.state.cardsActive){
-    return (
-      <div className="App">
-        <div className="Container">
-          {this.renderCards()}
-        </div>
-      </div>
-    )
-  }
+function App() {
   return (
     <div className="App">
-      <div className="Container">
-        <h1 className="Header"
-          >
-          {title}
-        </h1>
-        <div>
-          {this.getCard()}
-        </div>
-        <div className="ButtonContainer">
-          <button className="Button" onClick={() => this.setState({cardsActive: true,})}>The Cards</button>
-        </div>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-}
 }
 
 export default App;
